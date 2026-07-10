e5e672e5c7b1d91bb8ec14f858dcd369a39f539f
Allan <allan.gabriel587@gmail.com>
2026-07-09 21:32:07 -0300

    Atualizando o js

---
+++ js/produtos.js
@@ -0,0 +1,174 @@
+const produtos = [
+    // SEÇÃO 1: Corrida
+    {
+      id_produto: 1,
+      descricao_produto: "adidas2",
+      caminho_da_imagem: "../imagens/adidas2.webp",
+      valor_unitario: 699.90,
+      id_secao: 1,
+      nome_secao: "Corrida"
+    },
+    {
+      id_produto: 2,
+      descricao_produto: "balenciaga",
+      caminho_da_imagem: "../imagens/Balenciaga mendigo.jfif",
+      valor_unitario: 899.99,
+      id_secao: 1,
+      nome_secao: "Corrida"
+    },
+    {
+      id_produto: 3,
+      descricao_produto: "converse",
+      caminho_da_imagem: "..imagens/Converse.jpg",
+      valor_unitario: 799.90,
+      id_secao: 1,
+      nome_secao: "Corrida"
+    },
+    {
+      id_produto: 4,
+      descricao_produto: "crocs",
+      caminho_da_imagem: "../imagens/crocs.webp",
+      valor_unitario: 1199.90,
+      id_secao: 1,
+      nome_secao: "Corrida"
+    },
+  
+    // SEÇÃO 2: Casual
+    {
+      id_produto: 5,
+      descricao_produto: "crocs2",
+      caminho_da_imagem: "../imagens/crocs2.jfif",
+      valor_unitario: 349.90,
+      id_secao: 2,
+      nome_secao: "Casual"
+    },
+    {
+      id_produto: 6,
+      descricao_produto: "crogs",
+      caminho_da_imagem: "../imagens/Crogs.jfif",
+      valor_unitario: 249.90,
+      id_secao: 2,
+      nome_secao: "Casual"
+    },
+    {
+      id_produto: 7,
+      descricao_produto: "kenner",
+      caminho_da_imagem: "../imagens/kenner.webp",
+      valor_unitario: 799.99,
+      id_secao: 2,
+      nome_secao: "Casual"
+    },
+    {
+      id_produto: 8,
+      descricao_produto: "Nike Mind",
+      caminho_da_imagem: "..imagens/Nike Mind.webp",
+      valor_unitario: 499.90,
+      id_secao: 2,
+      nome_secao: "Casual"
+    },
+  
+    // SEÇÃO 3: Basquete
+    {
+      id_produto: 9,
+      descricao_produto: "tenisSalto",
+      caminho_da_imagem: "../imagens/Salto tenis.png",
+      valor_unitario: 1099.90,
+      id_secao: 3,
+      nome_secao: "Basquete"
+    },
+    {
+      id_produto: 10,
+      descricao_produto: "adidas de onca",
+      caminho_da_imagem: "../imagens/tenisdeOnca_Adidas.png",
+      valor_unitario: 849.90,
+      id_secao: 3,
+      nome_secao: "Basquete"
+    },
+    {
+      id_produto: 11,
+      descricao_produto: "tenisRatos",
+      caminho_da_imagem: "../imagens/tenis ratos.jpg",
+      valor_unitario: 999.90,
+      id_secao: 3,
+      nome_secao: "Basquete"
+    },
+    {
+      id_produto: 12,
+      descricao_produto: "tenisdCcorrida",
+      caminho_da_imagem: "../imagens/Tênis de corrida.jfif",
+      valor_unitario: 749.90,
+      id_secao: 3,
+      nome_secao: "Basquete"
+    },
+  
+    // SEÇÃO 4: Skate
+    {
+      id_produto: 13,
+      descricao_produto: "olympikus",
+      caminho_da_imagem: "../imagens/Tênis olympikus verde e azul.jfif",
+      valor_unitario: 899.90,
+      id_secao: 4,
+      nome_secao: "Skate"
+    },
+    {
+      id_produto: 14,
+      descricao_produto: "tenisTransparente",
+      caminho_da_imagem: "../imagens/Tênis transparente.jfif",
+      valor_unitario: 399.90,
+      id_secao: 4,
+      nome_secao: "Skate"
+    },
+    {
+      id_produto: 15,
+      descricao_produto: "yeezy",
+      caminho_da_imagem: "../imagens/Yeezy.webp",
+      valor_unitario: 329.90,
+      id_secao: 4,
+      nome_secao: "Skate"
+    },
+    {
+      id_produto: 16,
+      descricao_produto: "Tênis Adidas Matchbreak Super",
+      caminho_da_imagem: "../img/adidas_matchbreak.jpg",
+      valor_unitario: 449.90,
+      id_secao: 4,
+      nome_secao: "Skate"
+    },
+  
+    // SEÇÃO 5: Treino e Academia
+    {
+      id_produto: 17,
+      descricao_produto: "Tênis Nike Metcon 8",
+      caminho_da_imagem: "../img/nike_metcon_8.jpg",
+      valor_unitario: 899.99,
+      id_secao: 5,
+      nome_secao: "Treino e Academia"
+    },
+    {
+      id_produto: 18,
+      descricao_produto: "Tênis Reebok Nano X2",
+      caminho_da_imagem: "../img/reebok_nano_x2.jpg",
+      valor_unitario: 799.90,
+      id_secao: 5,
+      nome_secao: "Treino e Academia"
+    },
+    {
+      id_produto: 19,
+      descricao_produto: "Tênis Puma Fuse 2.0",
+      caminho_da_imagem: "../img/puma_fuse_2.jpg",
+      valor_unitario: 549.90,
+      id_secao: 5,
+      nome_secao: "Treino e Academia"
+    },
+    {
+      id_produto: 20,
+      descricao_produto: "Tênis Everlast Climber III",
+      caminho_da_imagem: "../img/everlast_climber.jpg",
+      valor_unitario: 299.90,
+      id_secao: 5,
+      nome_secao: "Treino e Academia"
+    }
+  ];
+  
+ 
+  export{produtos}
